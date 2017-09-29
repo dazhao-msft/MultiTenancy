e2e19ecb3d0d1a7a6971c00d59c427545796e486
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-09-29 22:46:39 +0000

    Adds back end service

--- MultiTenancy/Gateway/Controllers/ValuesController.cs
+++ MultiTenancy/Gateway/Controllers/ValuesController.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
 
 namespace Gateway.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public Task Get()
         {
-            return HttpContext.ProxyRequest(new Uri("http://google.com"));
+            return HttpContext.ProxyRequest(new Uri("http://localhost:44380/api/values"));
         }
     }
 }
---
+++ MultiTenancy/Service/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get() => "From Service";
+    }
+}
