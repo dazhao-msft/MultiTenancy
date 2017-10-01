6f1d2cb99c8db035944128d342cfa2dece461be6
David Zhao (DEVDIV) <dazhao@microsoft.com>
2017-10-01 02:16:36 +0000

    Adds tracing info

--- MultiTenancy/Gateway/Controllers/ValuesController.cs
+++ MultiTenancy/Gateway/Controllers/ValuesController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Gateway.Controllers
@@ -10,10 +12,21 @@ namespace Gateway.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
         // GET api/values
         [HttpGet]
         public Task Get()
         {
+            Claim appId = User.FindFirst("appid");
+            Claim tenantId = User.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid");
+            _logger.LogWarning("TraceId: {TraceId} | AppId: {AppId} | TenantId: {TenantId}", HttpContext.TraceIdentifier, appId.Value, tenantId.Value);
+
             // Service
             const string ServiceUrl = "http://localhost:44303/api/values";
 
